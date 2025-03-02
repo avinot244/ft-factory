7be353abdf9fa5229964f4be5b7af6d14281b1ff
avinot244 <aymericvinot38@gmail.com>
2025-03-02 14:47:05 +0100

    fixed data_loader

---
+++ data_loader.py
@@ -0,0 +1,18 @@
+from datasets import load_dataset
+from transformers.tokenization_utils_fast import PreTrainedTokenizerFast
+from datasets.arrow_dataset import Dataset
+
+
+
+
+def data_loader(tokenizer : PreTrainedTokenizerFast) -> Dataset:
+    dataset = load_dataset("avinot/LoL-Champion-Corpus-v2", split="train")
+    EOS_TOKEN = tokenizer.eos_token
+    
+    def formatting_prompts_func(examples : dict):
+        return { "text" : [example + EOS_TOKEN for example in examples["text"]] }
+    
+    dataset = dataset.map(formatting_prompts_func, batched=True)
+    print(type(dataset))
+    
+    
