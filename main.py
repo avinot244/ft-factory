4a702db1dc805a6b1073a93886f8f549f088196c
avinot244 <aymericvinot38@gmail.com>
2025-04-09 22:24:31 +0200

    training batch

--- main.py
+++ main.py
@@ -4,17 +4,18 @@ from services.huggingface.trainer_loader import trainer_hf
 from utils.globals import EPOCHS
 
 def main():
-    ft_mode = "lora"
-    model, tokenizer = get_model_and_tokenizer_hf("lora")
-    dataset_train = data_loader(tokenizer, "train")
-    # dataset = data_loader_eli5(tokenizer)
-    dataset_validation = data_loader(tokenizer, "validation")
-    model_name = f"LoLlama3.2-1B-{ft_mode}-{EPOCHS}ep"
-    
-    # trainer = trainer_hf(model_name, model, tokenizer, dataset["train"], dataset["test"])
-    trainer = trainer_hf(model_name, model, tokenizer, dataset_train, dataset_validation)
-    trainer.train()
-    trainer.push_to_hub()
+    for epochs in [5, 10, 20, 30, 40, 50]:
+        ft_mode = "lora"
+        model, tokenizer = get_model_and_tokenizer_hf("lora")
+        dataset_train = data_loader(tokenizer, "train")
+        # dataset = data_loader_eli5(tokenizer)
+        dataset_validation = data_loader(tokenizer, "validation")
+        model_name = f"LoLlama3.2-1B-{ft_mode}-{epochs}ep"
+        
+        # trainer = trainer_hf(model_name, model, tokenizer, dataset["train"], dataset["test"])
+        trainer = trainer_hf(model_name, model, tokenizer, dataset_train, dataset_validation)
+        trainer.train()
+        trainer.push_to_hub()
     
 
 if __name__ == "__main__":
