22d462ff13de625e15a352d59a94a238ee1ccb6f
avinot244 <aymericvinot38@gmail.com>
2025-04-16 01:02:41 +0200

    Update training epochs and model version for consistency across scripts

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from services.huggingface.trainer_loader import trainer_hf
 from utils.globals import EPOCHS
 
 def main():
-    for epochs in [70]:
+    for epochs in [50]:
         ft_mode = "lora"
         model, tokenizer = get_model_and_tokenizer_hf("lora")
         dataset_train = data_loader(tokenizer, "train")
         # dataset = data_loader_eli5(tokenizer)
         dataset_validation = data_loader(tokenizer, "validation")
-        model_name = f"LoLlama3.2-1B-{ft_mode}-{epochs}ep"
+        model_name = f"LoLlama3.2-1B-{ft_mode}-{epochs}ep-v2"
         
         # trainer = trainer_hf(model_name, model, tokenizer, dataset["train"], dataset["test"])
         trainer = trainer_hf(model_name, model, tokenizer, dataset_train, dataset_validation, epochs)
