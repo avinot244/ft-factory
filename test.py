c13a9499c3b37a2f1f3def9c3d0c2279c7b04694
avinot244 <aymericvinot38@gmail.com>
2025-05-08 15:47:55 +0200

    Add dataset loading and model training enhancements for instruction fine-tuning

--- test.py
+++ test.py
@@ -28,11 +28,12 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 
 base_model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-3.2-1B", torch_dtype=torch.float16)
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.2-1B")
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.2-1B-instruct")
+tokenizer.pad_token = tokenizer.eos_token
 model = PeftModel.from_pretrained(base_model, "avinot/Lollama3.2-1B-lora-3ep-v3")
 model = model.merge_and_unload()  # <<< Important line
 
-prompt = "Thresh is a champion in league of legend that can be"
+prompt = "What is the role of the champion Thresh ?"
 inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
 outputs = model.generate(**inputs, max_length=50, do_sample=False)
 print(tokenizer.decode(outputs[0], skip_special_tokens=True))
